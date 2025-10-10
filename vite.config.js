c810ecc9676002dd6d3c3fd1907f76874dc92ffe
nemeow0w0 <nemeow2002@hotmail.com>
2025-10-10 11:25:21 +0700

    upda confic vite

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-    base: '/'   
-=======
-  base: '/'
->>>>>>> 7b9ce18ba12e3e0c05d915ce0632283846abeac8
+  base: '/'   // หรือถ้า deploy GitHub Pages ให้ใส่ path ของ repo เช่น '/qr_code/'
 })
